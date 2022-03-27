0ca6f2ab8c27adcb1d6ba8bf13eedcf91df3b07e
Leung Cheng <monewckl@hotmail.com.hk>
2022-03-27 23:59:56 +0800

    add logic flow pseudo code

--- src/main/kotlin/GamingBoard.kt
+++ src/main/kotlin/GamingBoard.kt
@@ -2,7 +2,11 @@ const val BOAT_SIZE = 2
 
 typealias CrosserIndices = Set<Int>
 
-data class GamingState(val crossers: List<RiverCrosser>, val totalCost: Int = 0) {
+data class GamingState(
+    val crossers: List<RiverCrosser>,
+    val pastMoves: List<Pair<CrosserIndices, Move>>,
+    val totalCost: Int = 0,
+) {
     fun newStateAppliedMoves(crosserIndicesAndMove: Pair<CrosserIndices, Move>): GamingState {
         TODO()
     }
@@ -23,9 +27,35 @@ class GameSituationTeller(private val crossers: List<RiverCrosser>) {
 }
 
 class GamingBoard(crossers: List<RiverCrosser>) {
-    private val gamingStatesList = MutableList(1) { GamingState(crossers) }
+    private var transitedCrossersSet: Set<List<RiverCrosser>> = setOf()
 
-    fun getLowestCostGameSolvingPossibleMovesList(): Pair<CrosserIndices, Move> {
+    private val activeGamingStatesList =
+        MutableList(1) { GamingState(crossers, emptyList()) }
+    private val winGamingStatesWithLowestTotalCostList =
+        mutableListOf<GamingState>() //all totalCost are same in this list
+
+    fun getLowestCostGameSolvingPossibleMovesList(): List<Pair<CrosserIndices, Move>> {
         TODO()
+        /* For each state () in activeGamingStatesList:
+                obtain each valid moves (GameSituationTeller.getCurrentValidMoves)
+                futureStates = []
+                For each move:
+                    get new state after applied move (GamingSate.newStateAppliedMoves)
+                    if the new state's crossers not in transitedCrossersSet
+                        add to transitedCrossersSet
+                        if this new state is win (GameSituationTeller.isWin):
+                            if totalCost is equal to those in winGamingStatesWithLowestTotalCostList
+                                add to list
+                                skip this loop;
+                            if totalCost is less than those
+                                drop the winGameStatesList and let the list contain this new state only
+                            skip this loop;
+                        if totalCost is equal to those in winGamingStatesWithLowestTotalCostList
+                            skip this loop;
+                        if this new state is not game over (GameSituationTeller.isGameOver)
+                            add to futureStates
+                replace old state by futureStates in activeGamingStatesList
+           Repeat above until activeGamingStatesList is Empty
+        */
     }
 }
