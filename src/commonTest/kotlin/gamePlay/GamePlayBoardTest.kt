7620a89ac505fda77682d61adbfb74f36c0c608f
Leung Cheng <monewckl@hotmail.com.hk>
2023-05-13 18:50:31 +0800

    test update

--- src/commonTest/kotlin/gamePlay/GamePlayBoardTest.kt
+++ src/commonTest/kotlin/gamePlay/GamePlayBoardTest.kt
@@ -1,6 +1,7 @@
 package gamePlay
 
 import rules.Move
+import rules.RiverCrosserType
 import rules.classic.ClassicGameRules
 import rules.classic.FATHER
 import rules.classic.SON
@@ -10,24 +11,43 @@ import kotlin.test.assertEquals
 class GamePlayBoardTest {
 
     @Test
-    fun `test when one crosser who can drive boat on original riverside`() {
-        val actualMoves = GamePlayBoard.getMinCostGameSolvingMoves(
-            listOf(
-                FATHER
-            ), ClassicGameRules
+    fun `test getMinCostGameSolvingMoves when crossers can reach target riverside`() {
+        data class TestCase(
+            val crosserTypes: List<RiverCrosserType>,
+            val expectedMoves: Set<List<Pair<Set<Int>, Move>>>
         )
-        val expectedMoves = setOf(
-            listOf(
-                setOf(0) to Move.TRANSIT,
-                setOf(0) to Move.DRIVE_BOAT,
-                setOf(0) to Move.TRANSIT,
+
+        val testCases = listOf(
+            TestCase(
+                crosserTypes = listOf(FATHER), expectedMoves = setOf(
+                    listOf(
+                        setOf(0) to Move.TRANSIT,
+                        setOf(0) to Move.DRIVE_BOAT,
+                        setOf(0) to Move.TRANSIT,
+                    )
+                )
+            ),
+            TestCase(
+                crosserTypes = listOf(FATHER, SON), expectedMoves = setOf(
+                    listOf(
+                        setOf(0, 1) to Move.TRANSIT,
+                        setOf(0) to Move.DRIVE_BOAT,
+                        setOf(0, 1) to Move.TRANSIT,
+                    )
+                )
+            ),
+        )
+
+        testCases.forEach { testCase ->
+            val actualMoves = GamePlayBoard.getMinCostGameSolvingMoves(
+                testCase.crosserTypes, ClassicGameRules
             )
-        )
-        assertEquals(expectedMoves, actualMoves)
+            assertEquals(testCase.expectedMoves, actualMoves)
+        }
     }
 
     @Test
-    fun `test when one crosser who cannot drive boat on original riverside`() {
+    fun `test getMinCostGameSolvingMoves when one crosser who cannot drive boat on original riverside`() {
         val actualMoves = GamePlayBoard.getMinCostGameSolvingMoves(
             listOf(
                 SON
