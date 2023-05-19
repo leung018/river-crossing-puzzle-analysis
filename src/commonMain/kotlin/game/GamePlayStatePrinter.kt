3973dafdc3484807b91537cbaf24f60f6f205922
Leung Cheng <monewckl@hotmail.com.hk>
2023-05-19 18:01:23 +0800

    GameStatePrinter implement

--- src/commonMain/kotlin/game/GamePlayStatePrinter.kt
+++ src/commonMain/kotlin/game/GamePlayStatePrinter.kt
@@ -1,5 +1,8 @@
 package game
 
+import game.rules.BoatPosition
+import game.rules.RiverCrosserPosition
+
 
 interface Printer {
     fun print(log: String)
@@ -7,22 +10,78 @@ interface Printer {
 
 class ConsolePrinter : Printer {
     override fun print(log: String) {
-        println(log)
+        print(log)
     }
 }
 
 class OutputTracePrinter : Printer {
-    private val traces: MutableList<String> = mutableListOf()
+    private var trace: String = ""
 
     override fun print(log: String) {
-        traces.add(log)
+        trace += log
     }
 
-    fun getOutputTraces(): List<String> = traces
+    fun getOutputTrace(): String = trace
 }
 
 class GamePlayStatePrinter(private val printer: Printer) {
-    fun print(gamePlayState: GamePlayState) {
-        TODO()
+    fun printLog(gamePlayState: GamePlayState) {
+        val gamePlayPositionsLog = gamePlayPositionsLog(gamePlayState.gamePlayPositions)
+        myPrintln(gamePlayPositionsLog)
+        printDashLine(gamePlayPositionsLog.longestLineLength())
+        myPrintln("Total cost: ${gamePlayState.totalCost}")
+        myPrintln("Last move: ${gamePlayState.pastMoves.lastOrNull()?.let { moveLog(it) } ?: ""}")
     }
+
+    private fun myPrintln(log: String) {
+        printer.print(log + "\n")
+    }
+
+    private fun gamePlayPositionsLog(gamePlayPositions: GamePlayPositions): String {
+        val crossers = gamePlayPositions.crossers
+
+        return """
+            Original riverside |${
+            crossers.logOfPositionAt(RiverCrosserPosition.ORIGINAL_RIVERSIDE).addLeadingSpaceIfNotEmpty()
+        }
+            River              |${if (gamePlayPositions.boatPosition == BoatPosition.ORIGINAL_RIVERSIDE) " ${crossers.logOfBoat()}" else ""}
+            River              |${if (gamePlayPositions.boatPosition == BoatPosition.TARGET_RIVERSIDE) " ${crossers.logOfBoat()}" else ""}
+            Target riverside   |${
+            crossers.logOfPositionAt(RiverCrosserPosition.TARGET_RIVERSIDE).addLeadingSpaceIfNotEmpty()
+        }
+        """.trimIndent()
+    }
+
+    private fun String.addLeadingSpaceIfNotEmpty(): String {
+        return if (this.isNotEmpty()) {
+            " $this"
+        } else {
+            this
+        }
+    }
+
+    private fun List<RiverCrosser>.logOfBoat(): String = "<${this.logOfPositionAt(RiverCrosserPosition.BOAT)}>(Boat)"
+
+    private fun List<RiverCrosser>.logOfPositionAt(position: RiverCrosserPosition): String {
+        val tokens = mutableListOf<String>()
+        for ((index, crosser) in this.withIndex()) {
+            if (crosser.position == position) {
+                tokens.add("${crosser.type.id}(#${index + 1})")
+            }
+        }
+        return tokens.joinToString(", ")
+    }
+
+    private fun String.longestLineLength(): Int {
+        return split("\n").maxOfOrNull { it.length } ?: 0
+    }
+
+    private fun printDashLine(length: Int) {
+        myPrintln("-".repeat(length))
+    }
+
+    private fun moveLog(move: Move): String {
+        return "${move.crosserIndices.sorted().joinToString(", ") { "#${it + 1}" }} ${move.type}"
+    }
+
 }
--- src/commonTest/kotlin/game/GamePlayStatePrinterTest.kt
+++ src/commonTest/kotlin/game/GamePlayStatePrinterTest.kt
@@ -1,16 +1,15 @@
 package game
 
 import game.rules.BoatPosition
+import game.rules.MoveType
 import game.rules.RiverCrosserPosition
 import game.rules.RiverCrosserType
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class GamePlayStatePrinterTest {
     @Test
-    @Ignore
-    fun `test print`() {
+    fun `test printLog`() {
         data class TestCase(
             val gamePlayState: GamePlayState,
             val expectedOutput: String
@@ -28,19 +27,71 @@ class GamePlayStatePrinterTest {
                     ),
                     pastMoves = listOf(),
                     totalCost = 0,
-                ), expectedOutput = "TODO"
+                ), expectedOutput = """
+                    Original riverside | HANDSOME(#1)
+                    River              | <>(Boat)
+                    River              |
+                    Target riverside   | PRETTY(#2)
+                    ---------------------------------
+                    Total cost: 0
+                    Last move: 
+                """
             ),
+            TestCase(
+                gamePlayState = GamePlayState(
+                    gamePlayPositions = GamePlayPositions(
+                        crossers = listOf(
+                            RiverCrosser(RiverCrosserType("HANDSOME"), RiverCrosserPosition.BOAT),
+                            RiverCrosser(RiverCrosserType("PRETTY"), RiverCrosserPosition.BOAT),
+                        ),
+                        boatPosition = BoatPosition.TARGET_RIVERSIDE
+                    ),
+                    pastMoves = listOf(
+                        Move(setOf(1, 0), MoveType.TRANSIT),
+                        Move(setOf(1, 0), MoveType.DRIVE_BOAT),
+                    ),
+                    totalCost = 1,
+                ), expectedOutput = """
+                    Original riverside |
+                    River              |
+                    River              | <HANDSOME(#1), PRETTY(#2)>(Boat)
+                    Target riverside   |
+                    -----------------------------------------------------
+                    Total cost: 1
+                    Last move: #1, #2 DRIVE_BOAT
+                """
+            ),
+            TestCase(
+                gamePlayState = GamePlayState(
+                    gamePlayPositions = GamePlayPositions(
+                        crossers = listOf(
+                            RiverCrosser(RiverCrosserType("HANDSOME"), RiverCrosserPosition.BOAT),
+                        ),
+                        boatPosition = BoatPosition.ORIGINAL_RIVERSIDE
+                    ),
+                    pastMoves = listOf(
+                    ),
+                    totalCost = 0,
+                ),
+                expectedOutput = """
+                    Original riverside |
+                    River              | <HANDSOME(#1)>(Boat)
+                    River              |
+                    Target riverside   |
+                    -----------------------------------------
+                    Total cost: 0
+                    Last move: 
+                """
+            )
         )
 
         for (testCase in testCases) {
             val printer = OutputTracePrinter()
             val gamePlayStatePrinter = GamePlayStatePrinter(printer)
-            gamePlayStatePrinter.print(
+            gamePlayStatePrinter.printLog(
                 testCase.gamePlayState
             )
-            printer.getOutputTraces().last().let { trace ->
-                assertEquals(testCase.expectedOutput, trace)
-            }
+            assertEquals(testCase.expectedOutput.trimIndent() + "\n", printer.getOutputTrace())
         }
     }
 }
