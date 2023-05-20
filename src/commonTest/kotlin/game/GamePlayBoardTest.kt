100a488fca8111e36ed3f24bd9e46834c6d6927f
Leung Cheng <monewckl@hotmail.com.hk>
2023-05-20 19:27:45 +0800

    update test case comment style

--- src/commonTest/kotlin/game/GamePlayBoardTest.kt
+++ src/commonTest/kotlin/game/GamePlayBoardTest.kt
@@ -22,53 +22,64 @@ class GamePlayBoardTest {
                 crosserTypes = listOf(FATHER),
                 expectedTotalCost = 1,
                 expectedNumOfMoves = 3,
-                // sample answer:
-                // Move(setOf(0), MoveType.TRANSIT),
-                // Move(setOf(0), MoveType.DRIVE_BOAT),
-                // Move(setOf(0), MoveType.TRANSIT),
+                /**
+                 * sample answer:
+                 * Move(setOf(0), MoveType.TRANSIT),
+                 * Move(setOf(0), MoveType.DRIVE_BOAT),
+                 * Move(setOf(0), MoveType.TRANSIT),
+                 */
             ),
             TestCase(
                 crosserTypes = listOf(FATHER, SON),
                 expectedTotalCost = 1,
                 expectedNumOfMoves = 3,
-                // sample answer:
-                // Move(setOf(0, 1), MoveType.TRANSIT),
-                // Move(setOf(0, 1), MoveType.DRIVE_BOAT),
-                // Move(setOf(0, 1), MoveType.TRANSIT),
+                /**
+                 * sample answer:
+                 * Move(setOf(0, 1), MoveType.TRANSIT),
+                 * Move(setOf(0, 1), MoveType.DRIVE_BOAT),
+                 * Move(setOf(0, 1), MoveType.TRANSIT),
+                 */
 
             ),
             TestCase(
                 crosserTypes = listOf(FATHER, SON, SON),
                 expectedTotalCost = 3,
                 expectedNumOfMoves = 7,
-                // sample answer:
-                // Move(setOf(0, 1), MoveType.TRANSIT),
-                // Move(setOf(0, 1), MoveType.DRIVE_BOAT),
-                // Move(setOf(1), MoveType.TRANSIT),
-                // Move(setOf(0), MoveType.DRIVE_BOAT),
-                // Move(setOf(2), MoveType.TRANSIT),
-                // Move(setOf(0, 2), MoveType.DRIVE_BOAT),
-                // Move(setOf(0, 2), MoveType.TRANSIT)
+                /**
+                 * sample answer:
+                 * Move(setOf(0, 1), MoveType.TRANSIT),
+                 * Move(setOf(0, 1), MoveType.DRIVE_BOAT),
+                 * Move(setOf(1), MoveType.TRANSIT),
+                 * Move(setOf(0), MoveType.DRIVE_BOAT),
+                 * Move(setOf(2), MoveType.TRANSIT),
+                 * Move(setOf(0, 2), MoveType.DRIVE_BOAT),
+                 * Move(setOf(0, 2), MoveType.TRANSIT)
+                 */
             ),
             TestCase(
                 crosserTypes = listOf(FATHER, MOTHER, SON),
                 expectedTotalCost = 3,
                 expectedNumOfMoves = 7,
-                // sample answer:
-                // Move(setOf(0, 2), MoveType.TRANSIT),
-                // Move(setOf(0, 2), MoveType.DRIVE_BOAT),
-                // Move(setOf(2), MoveType.TRANSIT),
-                // Move(setOf(0), MoveType.DRIVE_BOAT),
-                // Move(setOf(1), MoveType.TRANSIT),
-                // Move(setOf(0, 1), MoveType.DRIVE_BOAT),
-                // Move(setOf(0, 1), MoveType.TRANSIT)
+                /**
+                 * sample answer:
+                 * Move(setOf(0, 2), MoveType.TRANSIT),
+                 * Move(setOf(0, 2), MoveType.DRIVE_BOAT),
+                 * Move(setOf(2), MoveType.TRANSIT),
+                 * Move(setOf(0), MoveType.DRIVE_BOAT),
+                 * Move(setOf(1), MoveType.TRANSIT),
+                 * Move(setOf(0, 1), MoveType.DRIVE_BOAT),
+                 * Move(setOf(0, 1), MoveType.TRANSIT)
+                 */
             ),
             TestCase(
                 crosserTypes = listOf(FATHER, MOTHER, SON, DAUGHTER, DAUGHTER, MASTER, DOG),
                 expectedTotalCost = 13,
                 expectedNumOfMoves = 32,
-            ) // Hard to deduce a sample answer for this by mental calculation. This test case is included because I use it to test the correctness of the algorithm.
-            // During bug fixing the algo, I found that the original algo is not optimal for this test case. The algo is fixed for a more optimal solution, and now include it to ensure test coverage.
+            )
+            /**
+             * Hard to deduce a sample answer for this by mental calculation. This test case is included because I use it to test the correctness of the algorithm.
+             * During bug fixing the algo, I found that the original algo is not optimal for this test case. The algo is fixed for a more optimal solution, and now include it to ensure test coverage.
+             */
         )
 
         testCases.forEach { testCase ->
